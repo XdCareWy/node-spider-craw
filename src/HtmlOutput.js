be6e823c7e626003a0df871e3f82a2191a7bc8b1
XdCareWy <aixu159@163.com>
2019-09-10 11:42:21 +0800

    chore: 格式化代码

--- src/HtmlOutput.js
+++ src/HtmlOutput.js
@@ -4,12 +4,12 @@ class HtmlOutput {
   constructor() {
     this.data = [];
   }
+
   collectData(data) {
     this.data.push(...data);
   }
 
   outputHtml() {
-    console.log(this.data)
     const trs = this.data.map(item => {
       return `<tr>
           <td>${item.title}</td>
@@ -17,16 +17,16 @@ class HtmlOutput {
         </tr>`;
     });
     const hmt = `<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-          <body>
-            <table>
+      <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+        </head>
+        <body>
+          <table>
             ${trs}
           </table>
         </body>
